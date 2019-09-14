bd910ed3c06452a9969b7c7f646eeb283edc88ee
Daniel Butler <dabutler89@gmail.com>
2019-09-14 13:51:28 -0400

    base functionality working

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask
+from threading import Lock
+
+from flask import Flask, render_template, session, request, copy_current_request_context
+from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
+
+# Set this variable to "threading", "eventlet", or "gevent" to test the different async modes, or leave it set to
+# None for the application to choose the best option based on installed packages.
+async_mode = None
 
 app = Flask(__name__)
+app.config['SECRET_KET'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response', {'data': 'Sever generated event', 'count': count}, namespace='/test')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('base.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
